34347a8a4dc380768995860f15b5bb4134c90410
Gyan Patra <gyanpatra@gmail.com>
2017-06-05 21:41:26 -0700

    Changing port to accomodate heroku deployment

--- index.js
+++ index.js
@@ -27,12 +27,13 @@ if (config.MONGOOSE_DEBUG) {
   });
 }
 
+const portToBeDeployed = process.env.PORT || config.port;
 // module.parent check is required to support mocha watch
 // src: https://github.com/mochajs/mocha/issues/1912
 if (!module.parent) {
   // listen on port config.port
-  app.listen(config.port, () => {
-    console.info(`server started on port ${config.port} (${config.env})`); // eslint-disable-line no-console
+  app.listen(portToBeDeployed, () => {
+    console.info(`server started on port ${portToBeDeployed} (${config.env})`); // eslint-disable-line no-console
   });
 }
 
